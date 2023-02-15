153c6d28a30b863e7bf9c2b7fcedd58b0f35e7c1
adonais <lihualove1@163.com>
2023-02-15 17:01:40 +0800

    rust code does not use lto

--- config/makefiles/rust.mk
+++ config/makefiles/rust.mk
@@ -90,7 +90,7 @@ ifndef MOZ_LTO_RUST_CROSS
 # Never enable when sancov is enabled to work around https://github.com/rust-lang/rust/issues/90300.
 ifndef rustflags_sancov
 # Never enable when coverage is enabled to work around https://github.com/rust-lang/rust/issues/90045.
-ifndef MOZ_CODE_COVERAGE
+ifndef MOZ_PGO
 ifeq (,$(findstring gkrust_gtest,$(RUST_LIBRARY_FILE)))
 cargo_rustc_flags += -Clto
 endif
